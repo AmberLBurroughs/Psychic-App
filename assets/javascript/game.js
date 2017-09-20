9139a71ad6b87143204ecae091c1301030e1418b
Amber Burroughs <amberlburroughs@gmail.com>
2017-09-20 12:10:50 -0700

    begin logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,35 +16,50 @@
 var wins = 0;
 var losses = 0;
 var totalGuesses = 5; // guesses left
-var lettersGuessed;
-var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var lettersGuessed = []; 
+const choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // constent
+var computerGuess = "a"; // for test
+// choices[Math.floor(Math.random() * choices.length)]
 // messages
 var newGameText =
         "<h2>Look deep into my crystal ball and predict the letter within...</h2>";
 var firstGuessWinText = 
-		  "<h2>Your Psychic abilities are insane! You knew the exact letter.</h2>";
+		  "<h2>Your psychic abilities are insane! You knew the exact letter.</h2>";
 var multiGuessWinText = 
-		  "<h2>It may have taken you</h2> " + totalGuesses + "<h2></h2>";
+		  "<h2>It only took you " + totalGuesses + " times to predict the letter.</h2>";
 var guessAgainText = 
-		  "<h3>Look deep into my crystal ball, guess what letter you will see?</h3>";
-var differentLetter = 
-		  "<h3>Look deep into my crystal ball, guess what letter you will see?</h3>";
-var playAgainText =
-		  "<h3>Look deep into my crystal ball, guess what letter you will see?</h3>";
+		  "<h3>Try another letter.</h3>";
+// var differentLetter = 
+// 		  "<h3></h3>";
+// var playAgainText =
+// 		  "<h3></h3>";
+		
 
-
-var someFunctionName = function(){
-	// show new game message
+var reset = function () {
 	document.querySelector("#message-section").innerHTML = newGameText;
-	// var userGuess;
+	var totalGuesses = 5; // guesses left
+	var lettersGuessed = []; 
 	var computerGuess = choices[Math.floor(Math.random() * choices.length)];
+ };
 
-	// document.querySelector("#answer").innerHTML = "<h1>" + computerGuess +"</h1>";
+var load = function () {
+    // show new game message
+	document.querySelector("#message-section").innerHTML = newGameText;
+	gameData();
+	currentGameData();
 };
-someFunctionName();
 
-// validate user input 
+var gameData = function () {
+	document.querySelector("#wins").innerHTML = wins;
+	document.querySelector("#losses").innerHTML = losses;
+};  
+
+var currentGameData = function () {
+	document.querySelector("#remaining-guesses").innerHTML = totalGuesses;	
+	document.querySelector("#incorrect-guesses").innerHTML = lettersGuessed;	
+}; // dont sure where to call this
+
+
 document.getElementById("letter-sub-btn").addEventListener("click", function(e){
 	var inpObj = document.getElementById("guess-form");
 	if (inpObj.checkValidity() == false) {
@@ -52,10 +67,66 @@ document.getElementById("letter-sub-btn").addEventListener("click", function(e){
 	} else {
 		e.preventDefault() 
 	}
-	var userGuess = document.getElementById("user-letter").value;
-    var patt =/[A-z]/g;
-    var isletter = patt.test(userGuess);
-    console.log(userGuess);
-    console.log(isletter);
+    if (totalGuesses <= 1) {
+    	document.getElementById("letter-sub-btn").setAttribute("disabled", true);
+    }
+	var userInput = document.getElementById("user-letter").value.toLowerCase();
+    psychic(userInput);
+    currentGameData();
+    document.getElementById("user-letter").value = ""; // reset input value
+    return false; // prevent page refresh
 });
 
+// ? 
+document.getElementById("user-letter").addEventListener("click", function(){
+	if (totalGuesses > 5 && totalGuesses < 0){
+		document.querySelector(".sub-messages").classList.add("hide");
+	}
+});
+
+var psychic = function(userInput){
+	var userGuess = userInput;
+	console.log(userGuess);
+	console.log(computerGuess);
+	// check if letters match
+	console.log(lettersGuessed);
+	if (computerGuess != userGuess) {
+		// check if guessed letter aleady
+		if (lettersGuessed.includes(userGuess)) {
+			// guess a diff letter text
+			return
+		} else {
+			lettersGuessed.push(userGuess);
+			if (totalGuesses > 0) {
+				totalGuesses--;
+				console.log(totalGuesses);
+				currentGameData();
+				document.querySelector(".sub-messages").innerHTML = guessAgainText;
+			} else {
+				// stop all
+				// play again button - reset all 
+				losses++;
+				gameData();
+			}
+		}
+	} else {
+		// check how many time took to get the right answer
+		if (totalGuesses === 5) {
+			document.querySelector("#message-section").innerHTML = firstGuessWinText;
+			document.querySelector("#answer").innerHTML = "<h1>" + computerGuess +"</h1>";
+			document.querySelector("#guess-form").classList.add("hide");
+			wins++;
+			gameData();
+		} else {
+			document.querySelector("#message-section").innerHTML = multiGuessWinText;
+			document.querySelector("#answer").innerHTML = "<h1>" + computerGuess +"</h1>";
+			document.querySelector("#guess-form").classList.add("hide");
+			wins++;
+			gameData();
+		}
+	}
+	currentGameData();
+};
+
+
+load();
